378ab6b73adf5f9355f578b5030cdc18354ee509
Cory Osborn <cory@coryosborn.com>
2011-10-16 15:27:36 -0700

    pulling puts debug statement

--- lib/geokit/geocoders.rb
+++ lib/geokit/geocoders.rb
@@ -147,8 +147,7 @@ module Geokit
       
       # Call the geocoder service using the timeout if configured.
       def self.call_geocoder_service(url)
-        puts url
-        Timeout::timeout(Geokit::Geocoders::request_timeout) { return self.do_get(url) } if Geokit::Geocoders::request_timeout        
+        Timeout::timeout(Geokit::Geocoders::request_timeout) { return self.do_get(url) } if Geokit::Geocoders::request_timeout
         return self.do_get(url)
       rescue TimeoutError
         return nil  
